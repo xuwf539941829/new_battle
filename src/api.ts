52e3cd733b6be25ed6ac2bc705f9a51d79f0e567
xuwf539941829 <57024086+xuwf539941829@users.noreply.github.com>
2026-03-20 10:57:42 +0800

    配置更新

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Assuming the API will be tested locally using the host's IP address.
-const API_BASE_URL = 'http://127.0.0.1:3000/api';
+const API_BASE_URL = 'http://192.168.1.10:3000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
